4d09c0fcaac7cd23fcf8ff49609d23cc0cf0e83f
Drew Tullos <tullos.andrew@gmail.com>
2024-04-05 22:52:06 -0500

    next config update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -14,6 +14,8 @@ import { unifiedConditional } from 'unified-conditional'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  // ADDED THIS FOR AWS
+  output: 'export',
 }
 
 function remarkMDXLayout(source, metaName) {
